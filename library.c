260da86abf5a1400fb2c3d5777e02888a67ff04c
Szymon Czap <nomyszeq@gmail.com>
2025-10-08 13:20:36 +0200

    Initial commit

---
+++ library.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    system("chcp 65001 > NUL");
+    printf("Polskie znaki: ąęłóźćńśŻŹĆ\n");
+    printf("Witaj Świecie\u263B\n");
+    printf("Naciśnij Enter, aby zakończyć...");
+    getchar();
+    return 0;
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef FIRSTTASK_LIBRARY_H
+#define FIRSTTASK_LIBRARY_H
+
+void hello(void);
+
+#endif // FIRSTTASK_LIBRARY_H
